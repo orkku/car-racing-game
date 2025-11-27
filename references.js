c78ec9868f820bd0ad1f9c35880318adedca4b4e
orkku <92727333+orkku@users.noreply.github.com>
2025-11-27 22:31:55 +0200

    typo fix #2

--- references.js
+++ references.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
 export const cars = {
     car1: // Dodge Charger (RWD)
-    {   body: './assets/cars/car1/Car_1_Body.fbx',
-        wheelFL: './assets/cars/car1/Car_1_Front_Left_Wheel.fbx',
-        wheelFR: './assets/cars/car1/Car_1_Front_Right_Wheel.fbx',
-        wheelRL: './assets/cars/car1/Car_1_Rear_Left_Wheel.fbx',
-        wheelRR: './assets/cars/car1/Car_1_Rear_Right_Wheel.fbx',
+    {   body: './assets/cars/car1/Car_1_Body.FBX',
+        wheelFL: './assets/cars/car1/Car_1_Front_Left_Wheel.FBX',
+        wheelFR: './assets/cars/car1/Car_1_Front_Right_Wheel.FBX',
+        wheelRL: './assets/cars/car1/Car_1_Rear_Left_Wheel.FBX',
+        wheelRR: './assets/cars/car1/Car_1_Rear_Right_Wheel.FBX',
         offsetFL: new THREE.Vector3(0.4, -0.2, 0.835),
         offsetFR: new THREE.Vector3(-0.4, -0.2, 0.835),
         offsetRL: new THREE.Vector3(0.4, -0.2, -0.74),
@@ -15,11 +15,11 @@ export const cars = {
         geometry: new THREE.Vector3(0.5, 0.25, 1.3)
     },
     car2: // Bugatti Chiron (RWD)
-    {   body: './assets/cars/car2/Car_2_Body.fbx',
-        wheelFL: './assets/cars/car2/Car_2_Front_Left_Wheel.fbx',
-        wheelFR: './assets/cars/car2/Car_2_Front_Right_Wheel.fbx',
-        wheelRL: './assets/cars/car2/Car_2_Rear_Left_Wheel.fbx',
-        wheelRR: './assets/cars/car2/Car_2_Rear_Right_Wheel.fbx',
+    {   body: './assets/cars/car2/Car_2_Body.FBX',
+        wheelFL: './assets/cars/car2/Car_2_Front_Left_Wheel.FBX',
+        wheelFR: './assets/cars/car2/Car_2_Front_Right_Wheel.FBX',
+        wheelRL: './assets/cars/car2/Car_2_Rear_Left_Wheel.FBX',
+        wheelRR: './assets/cars/car2/Car_2_Rear_Right_Wheel.FBX',
         offsetFL: new THREE.Vector3(0.525, -0.15, 0.755),
         offsetFR: new THREE.Vector3(-0.525, -0.15, 0.755),
         offsetRL: new THREE.Vector3(0.525, -0.15, -0.885),
@@ -29,11 +29,11 @@ export const cars = {
         geometry: new THREE.Vector3(0.6, 0.25, 1.3)
     },
     car4: // Porche 911 (RWD)
-    {   body: './assets/cars/car4/Car_4_Body.fbx',
-        wheelFL: './assets/cars/car4/Car_4_Front_Left_Wheel.fbx',
-        wheelFR: './assets/cars/car4/Car_4_Front_Right_Wheel.fbx',
-        wheelRL: './assets/cars/car4/Car_4_Rear_Left_Wheel.fbx',
-        wheelRR: './assets/cars/car4/Car_4_Rear_Right_Wheel.fbx',
+    {   body: './assets/cars/car4/Car_4_Body.FBX',
+        wheelFL: './assets/cars/car4/Car_4_Front_Left_Wheel.FBX',
+        wheelFR: './assets/cars/car4/Car_4_Front_Right_Wheel.FBX',
+        wheelRL: './assets/cars/car4/Car_4_Rear_Left_Wheel.FBX',
+        wheelRR: './assets/cars/car4/Car_4_Rear_Right_Wheel.FBX',
         offsetFL: new THREE.Vector3(0.425, -0.175, 0.7),
         offsetFR: new THREE.Vector3(-0.425, -0.175, 0.7),
         offsetRL: new THREE.Vector3(0.425, -0.175, -0.655),
@@ -43,11 +43,11 @@ export const cars = {
         geometry: new THREE.Vector3(0.5, 0.25, 1.2)
     },
     car10: // BMW M5 (RWD)
-    {   body: './assets/cars/car10/Car_10_Body.fbx',
-        wheelFL: './assets/cars/car10/Car_10_Front_Left_Wheel.fbx',
-        wheelFR: './assets/cars/car10/Car_10_Front_Right_Wheel.fbx',
-        wheelRL: './assets/cars/car10/Car_10_Rear_Left_Wheel.fbx',
-        wheelRR: './assets/cars/car10/Car_10_Rear_Right_Wheel.fbx',
+    {   body: './assets/cars/car10/Car_10_Body.FBX',
+        wheelFL: './assets/cars/car10/Car_10_Front_Left_Wheel.FBX',
+        wheelFR: './assets/cars/car10/Car_10_Front_Right_Wheel.FBX',
+        wheelRL: './assets/cars/car10/Car_10_Rear_Left_Wheel.FBX',
+        wheelRR: './assets/cars/car10/Car_10_Rear_Right_Wheel.FBX',
         offsetFL: new THREE.Vector3(0.4, -0.22, 0.81),
         offsetFR: new THREE.Vector3(-0.4, -0.22, 0.81),
         offsetRL: new THREE.Vector3(0.4, -0.22, -0.7),
